810f15a29dbd3d7411dd316746da3c9d171fda51
John Harris <39563774+harrisj09@users.noreply.github.com>
2020-01-23 12:18:57 -0500

    Made todo list of JS objectives

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,3 +2,17 @@
 window.addEventListener("load", (event) => {
     console.log("page loaded");
 });
+
+/* 
+Dom is needed for changing the color when hovering on app__redirects-options
+
+- Add data attributes to each one and use an event listener
+
+item.addEventListener("mouseover", () => {
+    item.style.color = #33A0FF;
+});
+
+JS functionality needed is
+- Change color of app__redirects
+- Hamburger menu for mobile versions
+*/
