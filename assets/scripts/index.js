5a4153c9ddb58d8759639a8c4b21819ef0575f9c
John Harris <39563774+harrisj09@users.noreply.github.com>
2020-01-30 12:19:35 -0500

    Created carousel objects

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,13 +1,31 @@
-// Newsletter on load for page
+function carousel(image, color, title, desc) {
+    this.image = image;
+    // Background color
+    this.color = color;
+    this.title = title;
+    this.desc = desc;
+}
+
+const caroselImages = [
+    new carousel("assets/images/iphonex.png", "linear-gradient(0.25turn, #FF4858, #8F65FF)", "iPhone X", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."),
+    new carousel("assets/images/iphone6.png", "#2E90E5", "iPhone 6", "Performance and design. Taken right to the edge.")
+];
+
 window.addEventListener("load", () => {
-    const entirePage = document.querySelector("html");
+    /* 
+    Add event listeners to get rid of the newsletter on startup, but if submit is pressed check the string length 
+    or check if it contains an "@" and ".com" in it.
+    */
 
-    // Handle style of newsletter in CSS
-    entirePage.innerHTML += `
-        <div class="app__newsletter">
-            
-        </div>
-    `;
-
-    
 });
+
+/*
+    Carousel effect
+
+    - Have an array of objects that contains
+        + Image used on car
+        + Style for background to apply
+        + BIO text
+    - Set a timer to switch between images
+    - If button is pressed to change image set the timer back to default wait time
+*/
